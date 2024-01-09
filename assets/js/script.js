3c8405da57e6794f237bbdfc20f589092f90aedd
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-01-09 18:03:56 +0000

    Added: Added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// setup the initial variables
 let taskData = {};
 const currentDay = dayjs().format('DD-MM-YY');
 
@@ -5,6 +6,7 @@ const currentDay = dayjs().format('DD-MM-YY');
 let savedData = localStorage.getItem('taskManager');
 if (savedData) {
     savedData = JSON.parse(savedData);
+    // check if the current day was already in the local storage
     if (savedData[currentDay]) {
         taskData = savedData[currentDay];
     } else {
@@ -14,12 +16,13 @@ if (savedData) {
     savedData = {};
 }
 
+// Update current day on the page and refresh the table
 $(document).ready(function () {
     $('#currentDay').text(dayjs().format('dddd, MMMM D'));
     refreshTable();
 });
 
-
+// refresh the table and update task input fields from local storage
 function refreshTable() {
     const rows = $('.row');
     let currentHour = Number(dayjs().format('H'));
@@ -38,27 +41,27 @@ function refreshTable() {
                 $(row.children[1]).val(savedData[currentDay][currentInput.id]);
             }
         }
-
-    })
+    });
 }
 
+// Add click event to the buttons and update task data
 $('.saveBtn').click(function (e) {
-
     const clickedInput = $(this).prev();
     const id = clickedInput.attr('id');
     const value = clickedInput.val();
+
+    // check if the task was changed and update task data in local storage
     if (value.trim()) {
         taskData[id] = value;
         savedData[currentDay] = taskData;
         localStorage.setItem('taskManager', JSON.stringify(savedData));
+        refreshTable();
     } else {
         if (savedData[currentDay][id]) {
             delete taskData[id];
             savedData[currentDay] = taskData;
             localStorage.setItem('taskManager', JSON.stringify(savedData));
+            refreshTable();
         }
-
     }
-})
-
-
+});
