c760b659e649ae2aefffb3bdb7f78697098b1d40
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-01-09 15:55:45 +0000

    Added:Updated refresh table function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,16 @@ $(document).ready(function () {
 
 function refreshTable() {
     const rows = $('.row');
-    let hour = dayjs().format('H');
-    for (row of rows) {
-        console.log(row);
-
-    }
+    let currentHour = Number(dayjs().format('H'));
+    rows.each(function(index, row) {
+        let currentInput = row.children[1];
+        if (Number(currentInput.id) < currentHour) {
+            $(row.children[1]).addClass('past');
+        }else if (Number(currentInput.id) === currentHour) {
+            $(row.children[1]).addClass('present');
+        }else {
+            $(row.children[1]).addClass('future');
+        }
+        
+    })
 }
