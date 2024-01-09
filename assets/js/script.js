20f5f4039856c7a08054cb5099da470bd6c5917b
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-01-09 17:11:15 +0000

    Added:Check if the local storage contains task manager data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const taskData = {};
 const currentDay = dayjs().format('DD-MM-YY');
-taskData[currentDay] = {};
+//taskData[currentDay] = {};
+
+// check if the task manager data exist in the local storage
+let savedData = localStorage.getItem('taskManager');
+if (savedData) {
+    savedData = JSON.parse(savedData);
+} else {
+    savedData ={};
+}
 
 $(document).ready(function () {
     $('#currentDay').text(dayjs().format('dddd, MMMM D'));
@@ -11,25 +19,25 @@ $(document).ready(function () {
 function refreshTable() {
     const rows = $('.row');
     let currentHour = Number(dayjs().format('H'));
-    rows.each(function(index, row) {
+    rows.each(function (index, row) {
         let currentInput = row.children[1];
         if (Number(currentInput.id) < currentHour) {
             $(row.children[1]).addClass('past');
-        }else if (Number(currentInput.id) === currentHour) {
+        } else if (Number(currentInput.id) === currentHour) {
             $(row.children[1]).addClass('present');
-        }else {
+        } else {
             $(row.children[1]).addClass('future');
         }
-        
+
     })
 }
 
-$('.saveBtn').click(function(e) {
-const clickedInput = $(this).prev();
-const id = clickedInput.attr('id');
-const value = clickedInput.val();
-taskData[currentDay][id] = value;
-console.log(taskData);
-console.log(id, value);
+$('.saveBtn').click(function (e) {
+    const clickedInput = $(this).prev();
+    const id = clickedInput.attr('id');
+    const value = clickedInput.val();
+    if (value) {
+        taskData[currentDay][id] = value;
+    }
 })
 
