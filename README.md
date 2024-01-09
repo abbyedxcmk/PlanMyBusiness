ae96579574fff0327b2a525ade424efca0672a70
Isiaka Bhadmus <swiftnetuk@icloud.com>
2024-01-09 07:52:16 +0000

    Added: html and css for a row

---
+++ README.md
@@ -0,0 +1,2 @@
+# Plan My Business
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-    console.log(dayjs().format('DD/MM/YYYY')) // '25/01/2019'
     $('#currentDay').text(dayjs().format('dddd, MMMM D'));
-
-
 });
 
 
